13f73b291d0154d2dbefe995d1b76fe286bc11ed
SpaceMonkeyy86 <jackreynolds9128@gmail.com>
2025-09-21 11:02:24 -0400

    Write the mod

--- DisableAchievementsPlugin.cs
+++ DisableAchievementsPlugin.cs
@@ -1,14 +1,44 @@
 using BepInEx;
+using HarmonyLib;
 
 namespace Silksong.DisableAchievements;
 
-// TODO - adjust the plugin guid as needed
-[BepInAutoPlugin(id: "io.github.silksong_disableachievements")]
+[BepInAutoPlugin(id: "com.spacemonkeyy.silksong_disableachievements")]
 public partial class DisableAchievementsPlugin : BaseUnityPlugin
 {
     private void Awake()
     {
+        Harmony harmony = new(Id);
+        harmony.PatchAll();
+
         // Put your initialization logic here
         Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
+    }
+}
+
+[HarmonyPatch(typeof(AchievementHandler), nameof(AchievementHandler.AwardAchievementToPlayer))]
+public class AchievementHandler_AwardAchievementToPlayer_Patch
+{
+    public static bool Prefix()
+    {
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(AchievementHandler), nameof(AchievementHandler.UpdateAchievementProgress))]
+public class AchievementHandler_UpdateAchievementProgress_Patch
+{
+    public static bool Prefix()
+    {
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(MenuStyleUnlock), nameof(MenuStyleUnlock.Unlock))]
+public class MenuStyleUnlock_Unlock_Patch
+{
+    public static bool Prefix()
+    {
+        return false;
     }
 }
